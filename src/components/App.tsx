0ab8f11055354271f934cf99c27a3f97fb230d72
Mubarak <mubarakali.00@gmail.com>
2021-01-14 19:37:15 +0800

    Removed the commented code.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,11 @@
 import "react-native-gesture-handler";
 import React from "react";
-import { View, Text } from "react-native";
 import { enableScreens } from "react-native-screens";
+
 import Routes from "routes/Routes";
 
 enableScreens();
 
 export default () => {
-    return (
-        <Routes />
-        // <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        //     <Text style={{ fontSize: 17 }}>
-        //         Hello there, <Text style={{ fontWeight: "bold" }}>How are you?</Text>
-        //     </Text>
-        // </View>
-    );
+    return <Routes />;
 };
